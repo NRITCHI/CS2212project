2f10882340c69282d2d66885157912470b861b86
Harguun Singh Dogra <hdogra@uwo.ca>
2023-11-30 00:15:35 +0000

    load and save methods

--- src/File.java
+++ src/File.java
@@ -5,10 +5,28 @@ public class File {
     }
 
     public static void Load(){
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Text File","txt"));
+        fileChooser.setAcceptAllFileFilterUsed(false);
 
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION){
+            String path = fileChooser.getSelectedFile().getAbsolutePath();
+            if(!path.endsWith(".txt")){
+                JOptionPane.showMessageDialog(null, "Unable to read file\nPlease choose a .txt file");
+            }
+        }
     }
 
     public static void Save(){
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Text File","txt"));
+        fileChooser.setAcceptAllFileFilterUsed(false);
 
+        int result = fileChooser.showSaveDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION){
+            String path = fileChooser.getSelectedFile().getAbsolutePath();
+            if(!path.endsWith(".txt")){
+                path += ".txt";
+            }
+        }
     }
 }
