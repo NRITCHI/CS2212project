40553b5dac3467aca38a29b282f2e2b228341e10
Nathan Ritchie <nritchi4@uwo.ca>
2023-11-30 21:41:31 -0500

    Added more tests

--- testing/CorrectionTest.java
+++ testing/CorrectionTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CorrectionTest {
@@ -12,8 +14,41 @@ class CorrectionTest {
     }
 
     @Test
-    void checkSpelling() {
+    void getCorrectionsEmptyInput() {
+        assertEquals(0, Correction.GetCorrections("").length);
+    }
+
+    // Test for input with only numbers
+    @Test
+    void getCorrectionsNumbersOnly() {
+        assertEquals(0, Correction.GetCorrections("12345").length);
+    }
+
+    // Test for input with special characters
+    @Test
+    void getCorrectionsSpecialCharacters() {
+        assertEquals(0, Correction.GetCorrections("!@#$%").length);
+    }
+
+    @Test
+    void checkSpellingExtraLetter() {
         String[] correct = Correction.CheckSpelling("texxt");
+        for(int i = 0; i < correct.length; i++){
+            if (correct[i].equals("text")) assert true;
+        }
+    }
+
+    @Test
+    void checkSpellingLessLetter() {
+        String[] correct = Correction.CheckSpelling("tet");
+        for(int i = 0; i < correct.length; i++){
+            if (correct[i].equals("text")) assert true;
+        }
+    }
+
+    @Test
+    void checkSpellingWrongLetter() {
+        String[] correct = Correction.CheckSpelling("tert");
         for(int i = 0; i < correct.length; i++){
             if (correct[i].equals("text")) assert true;
         }
